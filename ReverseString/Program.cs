3064cbba0b7aaf0f128ddb3c6d21dfaafa0ecb1b
unknown <alexandrialware@gmail.com>
2018-02-03 11:42:49 -0500

    initial commit

---
+++ ReverseString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string forwardString = "Hello world!";
+            string[] words = forwardString.Split();
+            
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+
+        }
+    }
+}
